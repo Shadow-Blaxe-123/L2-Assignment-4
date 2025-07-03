0552f14c37b43bf4949ef09c70eb65e93fd390a7
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2025-07-04 01:40:58 +0600

    Added Redux Toolkit

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,3 +1,7 @@
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-gray-800 p-4 text-sm text-gray-500">
+      MinLibary™ . All Rights Reserved.
+    </footer>
+  );
 }
